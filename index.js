8a2efbfe111df680546e4e5818f709b0d5aa98bb
SteveA28 <alonsosteven32@gmail.com>
2023-12-02 13:26:33 -0500

    added inquirer prompt for readme

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ ${data.usage}
 
 ## License
 This project is licensed under the ${data.license} license.
-![License Badge](https://img.shields.io/badge/license-${encodeURIComponent(data.license)}-green)
+![License Badge](https://img.shields.io/badge/license-${encodeURIComponent(
+    data.license
+  )}-green)
 
 ## Contributing
 ${data.contributing}
@@ -36,3 +38,42 @@ For any questions, please contact:
 - GitHub: [${data.username}](https://github.com/${data.username})
 - Email: ${data.email}`;
 }
+
+inquirer
+  .prompt([
+    { type: "input", name: "title", message: "Enter project title:" },
+    {
+      type: "input",
+      name: "description",
+      message: "Enter project description:",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Enter installation instructions:",
+    },
+    { type: "input", name: "usage", message: "Enter usage information:" },
+    {
+      type: "list",
+      name: "license",
+      message: "Choose a license:",
+      choices: ["MIT", "GPL", "Apache", "BSD", "None"],
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "Enter contribution guidelines:",
+    },
+    { type: "input", name: "tests", message: "Enter test instructions:" },
+    { type: "input", name: "username", message: "Enter GitHub username:" },
+    { type: "input", name: "email", message: "Enter email address:" },
+  ])
+  .then((answers) => {
+    const readmeContent = generateREADME(answers);
+
+    fs.writeFile("README.md", readmeContent, (err) => {
+      if (err) throw err;
+      console.log("README.md has been successfully created!");
+    });
+  })
+  .catch((error) => console.error(error));
